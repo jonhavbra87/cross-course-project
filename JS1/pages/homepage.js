45b1c163d422f9efed764b6830c5a6eb422e0601
h-bcode <jon.are.br@gmail.com>
2023-12-04 18:51:22 +0100

    v10.1
    design changes

--- JS1/pages/homepage.js
+++ JS1/pages/homepage.js
@@ -2,11 +2,11 @@
 
 export async function homePage() {
 
-    const homeContent = document.querySelector(".homeContent");
+    const homeContent = document.querySelector(".home-content");
 
     if (homeContent) {
       homeContent.innerHTML = `
-<div><img src="/images/gamer.jpg" alt="gamer in front of a computer"></img></div>
+<div><img src="/images/gamer.jpg" alt="gamer in front of a computer" class="rounded"></img></div>
 <br>
 <div><button class="productsbutton" onclick="window.location.href ='./products/'">Visit our products</button></div>
                                 `;
